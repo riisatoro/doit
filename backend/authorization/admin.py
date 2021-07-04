b28a2d9fd6b2eebb376cb8217ebd68c097e1c5ab
riisatoro <riisa.toro@gmail.com>
2021-07-04 23:21:11 +0300

    Add CustomUser to dashboard

--- backend/authorization/admin.py
+++ backend/authorization/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from db_models.models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(CustomUser, CustomUserAdmin)
