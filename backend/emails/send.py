29a905d2893b0a62201c8ac9a27f9ddb4edfb1c4
riisatoro <riisa.toro@gmail.com>
2021-07-04 23:21:40 +0300

    Update send_email

--- backend/emails/send.py
+++ backend/emails/send.py
@@ -4,12 +4,12 @@ import requests
 
 
 
-def send_email(recepients: list):
+def send_email(recepients: list, token: str):
     return requests.post(
         f'https://api.mailgun.net/v3/{settings.MAILGUN_DOMAIN}/messages',
         auth=("api", settings.MAILGUN_API_KEY),
         data={"from": settings.MAILGUN_FROM,
             "to": recepients,
-            "subject": "Hello",
-            "text": "Testing some Mailgun awesomness!"}
+            "subject": "Confirm your email on DOIT stock!",
+            "text": f"Please, confirm your email: http://localhost:8000/auth/confirm/{token}/"}
         )
