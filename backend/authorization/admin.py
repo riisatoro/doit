eaa07207e412d07819b95ea7ea7346c4211cd45d
riisatoro <riisa.toro@gmail.com>
2021-07-05 23:29:58 +0300

    Add AutoSLugField

--- backend/authorization/admin.py
+++ backend/authorization/admin.py
@@ -3,6 +3,6 @@ from db_models.models import CustomUser
 
 # Register your models here.
 class CustomUserAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['username', 'email', 'slug', 'is_active']
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- backend/db_models/models.py
+++ backend/db_models/models.py
@@ -4,9 +4,11 @@ from django.db.models import (
     EmailField,
     DateTimeField,
     ForeignKey,
+    SlugField,
     SET,
 )
 from django.contrib.auth.models import AbstractUser
+from autoslug import AutoSlugField
 
 
 class UserType(Model):
@@ -23,6 +25,9 @@ class CustomUser(AbstractUser):
     username = CharField(blank=False, null=False, unique=True, max_length=50)
     email = EmailField(blank=False, null=False, unique=True)
     user_type = ForeignKey(to=UserType, blank=False, null=False, on_delete=SET(get_or_create_user_type))
+    slug = AutoSlugField(
+        max_length=100, unique=True, blank=False, null=False,  populate_from='username',
+    )
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
 
