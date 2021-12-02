dd6d2e31b99df1528bf03047d2a2720c6f16aac1
riisatoro <riisa.toro@gmail.com>
2021-12-02 08:41:15 +0200

    dev: user archive orders view

--- backend/stock/views.py
+++ backend/stock/views.py
@@ -29,6 +29,7 @@ class StockOrderListView(ListAPIView):
 
 
 class StockOrderView(APIView):
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, slug):
         order = get_object_or_404(StockOrder, slug=slug)
@@ -42,20 +43,36 @@ class StockOrderView(APIView):
 
 
 class ApplyToOrderView(APIView):
-    ...
+    permission_classes = (IsAuthenticated,)
 
 
 class SubmitSolutionView(APIView):
-    ...
+    permission_classes = (IsAuthenticated,)
 
 
 class ConfirmApplicantView(APIView):
-    ...
+    permission_classes = (IsAuthenticated,)
 
 
 class ConfirmFinishOrderView(APIView):
-    ...
-
+    permission_classes = (IsAuthenticated,)
+    
 
 class StockOrderArchiveView(APIView):
-    ...
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        
+        my_orders = StockOrder.objects.filter(author=user)
+        applied_order = StockOrder.objects.filter(applicants__in=[user])
+        in_progress = StockOrder.objects.filter(executor=user).exclude(order_status=OrderStatus.FINISHED)
+        executed_orders = StockOrder.objects.filter(executor=user, order_status=OrderStatus.FINISHED)
+
+        data = {
+            'my': OrderListSerializer(my_orders, many=True).data,
+            'applied': OrderListSerializer(applied_order, many=True).data,
+            'in_progress': OrderListSerializer(in_progress, many=True).data,
+            'executed': OrderListSerializer(executed_orders, many=True).data,
+        }
+        return Response(data)
